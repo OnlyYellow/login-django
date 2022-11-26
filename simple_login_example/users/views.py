040781a4aa0fe92adf73579c3d796c86b658c97f
OnlyYellow <ash1823@naver.com>
2022-11-26 11:49:18 +0900

    8주차 실습 완료

--- simple_login_example/users/views.py
+++ simple_login_example/users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest
 
 from django.utils.datastructures import MultiValueDictKeyError
@@ -17,6 +17,11 @@ def login(request):
     
     # username = request.GET['username']
     # password = request.GET['password']
+    context = {
+        'method': request.method,
+        'is_valid': True
+    }
+    
     
     if (request.method == 'GET'):
         # 이하 주석 처리된 부분은 삭제해도 되는 부분
@@ -27,7 +32,7 @@ def login(request):
         # # username과 password중 하나의 입력이 누락되었을 때
         # elif username is None or password is None:
         #     return HttpResponse('불가능한 접근입니다.')
-        return render(request, 'users/login.html')
+        return render(request, 'users/login.html', context)
     
     if (request.method == 'POST'):
         # username, password = None, None
@@ -49,19 +54,35 @@ def login(request):
         
         #blank 상태일 때는 유저가 input을 입력하지 않고 제출했을 때(유저의 실수)
         if username == '':
-            return HttpResponse('유저 아이디를 입력해주세요')
+            # return HttpResponse('유저 아이디를 입력해주세요')
+            context['is_valid'] = False
         if password == '':
-            return HttpResponse('유저 비밀번호를 입력해주세요')
+            # return HttpResponse('유저 비밀번호를 입력해주세요')
+            context['is_valid'] = False
         
         if (username != user_data['username']):
-            return HttpResponse('유저 아이디가 올바르지 않습니다.')
+            # return HttpResponse('유저 아이디가 올바르지 않습니다.')
+            context['is_valid'] = False
         if (password != user_data['password']):
-            return HttpResponse('유저 비밀번호가 올바르지 않습니다.')
+            # return HttpResponse('유저 비밀번호가 올바르지 않습니다.')
+            context['is_valid'] = False
         # return render(request, 'users/login.html')
     
+        if context['is_valid']:
+            # return redirect('pages:index')
+            response = redirect('pages:index')
+            
+            response.set_cookie('username', user_data['username'])
+            response.set_cookie('password', user_data['password'])
+            response.set_cookie('is_login', True)
+            
+            return response
         # return HttpResponse('로그인 성공!!')
-        return render(request, 'users/login_success.html')
+        return render(request, 'users/login.html', context)
     return HttpResponse()
 
 def login_detail(request, id):
     return HttpResponse('user id는 ' + str(id) + '입니다.')
+
+def index(request,):
+    return render(request, 'index.html')
